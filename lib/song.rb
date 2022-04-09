a48789261ec4520aa3f96a49c358e24bdf7638dd
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-09 14:08:43 -0700

    commit tree e72decd34d699014c16198d412d28ce479f84b9e

---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Song 
+    @@count = 0
+    @@genres = []
+    @@genre_count = {}
+    @@artists = []
+    @@arist_count = {}
+    attr_accessor :name, :artist, :genre
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+    
+    def self.genre_count
+        genre_hash = {}
+        @@genres.each do |genre|
+            if genre_hash.include? genre
+                genre_hash[genre] += 1
+            else genre_hash[genre] = 1
+            end
+        end
+        genre_hash
+    end
+
+    def self.artist_count
+        artist_hash = {}
+        @@artists.each do |artist|
+            if artist_hash.include? artist
+                artist_hash[artist] += 1
+            else artist_hash[artist] = 1
+            end
+        end
+        artist_hash
+    end
+end
+
+
+Song.new("Lucifer", "Jay-Z", "rap")
+Song.new("hit me baby one more time", "Brittany Spears", "pop")
+Song.new("99 Problems", "Jay-Z", "rap")
+
+puts Song.genre_count
